77dcd882dee34c6f1e9a544580687553f0eb526d
宇山拓夢 <jf1vax@gmail.com>
2016-09-14 14:30:12 +0900

    立方体の追加

--- js/application.js
+++ js/application.js
@@ -1,6 +1,20 @@
 window.onload = function() {
   var renderer = new THREE.WebGLRenderer({antialias: true});
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(40, 800 / 480);
+  var cube = new THREE.CubeGeometry(1, 1, 1);
+  var material = new THREE.MeshPhongMaterial({color: 0x965200});
+  var mesh = new THREE.Mesh(cube, material);
+  
   renderer.setSize(800, 480);
   renderer.setClearColor(0xffffff);
   document.getElementById("canvas_wrapper").appendChild(renderer.domElement);
+
+  camera.position.set(0, 0, 0);
+  camera.lookAt(new THREE.Vector3(1, 0, 0));
+
+  mesh.position.set(5, 0, 0);
+  scene.add(mesh);
+
+  renderer.render(scene, camera);
 }
