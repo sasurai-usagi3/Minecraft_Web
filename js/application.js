51bc13b2d6fa6419a0304c9eba8f8062e4419c2b
宇山拓夢 <jf1vax@gmail.com>
2016-09-15 13:52:27 +0900

    複数のブロックを出してみる

--- js/application.js
+++ js/application.js
@@ -3,8 +3,6 @@ window.onload = function() {
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera(40, 800 / 480);
   var cube = new THREE.CubeGeometry(1, 1, 1);
-  var dirt = Blocks.dirt;
-  var mesh = new THREE.Mesh(cube, dirt.getMaterial());
   var light = new THREE.AmbientLight(0xffffff);
   var updateCanvas = function() {
     requestAnimationFrame(updateCanvas);
@@ -15,11 +13,24 @@ window.onload = function() {
   renderer.setClearColor(0xffffff);
   document.getElementById("canvas_wrapper").appendChild(renderer.domElement);
 
-  camera.position.set(0, 0, 0);
-  camera.lookAt(new THREE.Vector3(1, 0, 0));
+  camera.position.set(0, 4, 15);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-  mesh.position.set(5, 0, 0);
-  scene.add(mesh);
+  for(var x = -3; x <= 3; ++x) {
+    for(var z = -3; z <= 3; ++z) {
+      var meshDirt = new THREE.Mesh(cube, Blocks.dirt.getMaterial());
+      var meshRock = new THREE.Mesh(cube, Blocks.rock.getMaterial());
+      var meshBedrock = new THREE.Mesh(cube, Blocks.bedrock.getMaterial());
+
+      meshDirt.position.set(x, 0, z);
+      meshRock.position.set(x, -1, z);
+      meshBedrock.position.set(x, -2, z);
+      
+      scene.add(meshDirt);
+      scene.add(meshRock);
+      scene.add(meshBedrock);
+    }
+  }
 
   scene.add(light);
 
