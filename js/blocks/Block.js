457d3e343dd7ef688412a1738a668f84e9e4dd4e
宇山拓夢 <sasurai.usagi3@gmail.com>
2016-09-28 10:25:13 +0900

    block名をインスタンス変数から読み出し

--- js/blocks/Block.js
+++ js/blocks/Block.js
@@ -7,7 +7,7 @@ class Block {
   }
 
   setTextures() {
-    let texture = new THREE.TextureLoader().load("/img/textures/blocks/" + blockName + ".png");
+    let texture = new THREE.TextureLoader().load("/img/textures/blocks/" + this.name + ".png");
     this.material = new THREE.MeshBasicMaterial({color: 0xffffff, map: texture});
   }
 
